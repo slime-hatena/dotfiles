3807744ef60e67f3c2a029b5ae920502fcd9ef86
Slime-hatena <git@aki-memo.net>
2021-07-21 02:45:47 +0900

    fix: 権限の不足を修正

--- scripts/install.sh
+++ scripts/install.sh
@@ -129,9 +129,9 @@ install() {
             cd portable-ruby
             sudo ln -sf 2.6.3 current
 
-            git clone https://github.com/Homebrew/brew ~/.linuxbrew/Homebrew
+            sudo git clone https://github.com/Homebrew/brew ~/.linuxbrew/Homebrew
             mkdir ~/.linuxbrew/bin
-            ln -s ~/.linuxbrew/Homebrew/bin/brew ~/.linuxbrew/bin
+            sudo ln -s ~/.linuxbrew/Homebrew/bin/brew ~/.linuxbrew/bin
             echo 'eval "$(~/.linuxbrew/bin/brew shellenv)"' >>$HOME/.bash_path
             eval $(~/.linuxbrew/bin/brew shellenv)
 
