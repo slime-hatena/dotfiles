30ecea760dd3237d594006c484624933516a1cb5
Slime-hatena <git@aki-memo.net>
2022-04-02 02:47:06 +0900

    feat: winget用のコマンドを追加

---
+++ windows/winget.ps1
@@ -0,0 +1,16 @@
+winget install -e --id Bitwarden.Bitwarden;
+winget install -e --id Logitech.GHUB;
+winget install -e --id Microsoft.PowerToys;
+winget install -e --id Nvidia.GeForceExperience;
+winget install -e --id Google.Chrome;
+winget install -e --id Mozilla.Thunderbird;
+winget install -e --id SlackTechnologies.Slack;
+winget install -e --id Discord.Discord;
+winget install -e --id Docker.DockerDesktop;
+winget install -e --id Microsoft.VisualStudioCode;
+winget install -e --id Microsoft.VisualStudioCode.Insiders;
+winget install -e --id TablePlus.TablePlus;
+winget install -e --id Microsoft.PowerShell;
+winget install -e --id Valve.Steam;
+winget install -e --id CrystalDewWorld.CrystalDiskInfo.ShizukuEdition;
+winget install -e --id CrystalDewWorld.CrystalDiskMark.ShizukuEdition;
