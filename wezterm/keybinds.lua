bf92f2bc2daed2ac6f7906a6c12a66add8d9c459
slime-hatena <git@aki-memo.net>
2024-11-26 13:11:15 +0900

    feat: update wezterm

--- wezterm/keybinds.lua
+++ wezterm/keybinds.lua
@@ -7,6 +7,8 @@ return {
     { key = 'v', mods = 'SUPER', action = wezterm.action.PasteFrom 'Clipboard' },
     { key = 'k', mods = 'SUPER', action = wezterm.action.ClearScrollback 'ScrollbackOnly' },
     { key = 'k', mods = 'SUPER|SHIFT', action = wezterm.action.ClearScrollback 'ScrollbackAndViewport' },
+    { key = '+', mods = 'SUPER', action = wezterm.action.IncreaseFontSize },
+    { key = '-', mods = 'SUPER', action = wezterm.action.DecreaseFontSize },
   },
   key_tables = {
     copy_mode = {
