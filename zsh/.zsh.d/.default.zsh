a85654ab3de501e1b471f87470e0563b1f373785
slime-hatena <git@aki-memo.net>
2025-01-24 18:15:21 +0900

    feat(zsh): change history options

--- zsh/.zsh.d/.default.zsh
+++ zsh/.zsh.d/.default.zsh
@@ -1,14 +1,21 @@
 INTERACTIVE_FILTER="fzf-tmux"
 
+unsetopt nomatch
+
 # macOS: enable ^q ^s etc...
 setopt no_flow_control
 
+export HISTSIZE=100000
+export SAVEHIST=100000
+setopt hist_ignore_dups     # 前と重複する行は記録しない
+setopt share_history        # 同時に起動したzshの間でヒストリを共有する
+setopt hist_reduce_blanks   # 余分なスペースを削除してヒストリに保存する
+setopt HIST_IGNORE_SPACE    # 行頭がスペースのコマンドは記録しない
+setopt HIST_IGNORE_ALL_DUPS # 履歴中の重複行をファイル記録前に無くす
+setopt HIST_FIND_NO_DUPS    # 履歴検索中、(連続してなくとも)重複を飛ばす
+setopt HIST_NO_STORE        # histroyコマンドは記録しない
+
 setopt GLOB_SUBST
-setopt HIST_IGNORE_DUPS
-setopt HIST_IGNORE_SPACE
-setopt INC_APPEND_HISTORY
-setopt HIST_EXPIRE_DUPS_FIRST
-setopt SHARE_HISTORY
 
 DISABLE_AUTO_TITLE="true"
 
