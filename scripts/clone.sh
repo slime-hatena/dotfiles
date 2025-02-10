65c19d4242ef95d937a4fee74a8b42792b22dff6
slime-hatena <git@aki-memo.net>
2025-02-10 15:26:01 +0900

    fix: 不要な関数を削除

--- scripts/clone.sh
+++ scripts/clone.sh
@@ -14,27 +14,6 @@ exists() {
     type "$1" >/dev/null 2>&1
 }
 
-### ファイルが存在する場合は待避し、シンボリックリンクを作成します。
-create_symbolic() {
-    info "${1} → ${2} のシンボリックリンクを作成します。"
-    if [ -e "$2" ]; then
-        if [ -L "$2" ]; then
-            warn "シンボリックリンクが存在します。削除します。"
-            rm "$2"
-        elif [ -d $2 ]; then
-            filename=$(basename $2).$(date +%s)
-            info "ディレクトリが存在します。${dotfilesDirectory}/backup/${filename}に移動します。"
-            mkdir -p "${dotfilesDirectory}/backup/${filename}"
-            mv "$2" "${dotfilesDirectory}/backup/${filename}"
-        else
-            filename=$(basename $2).$(date +%s)
-            info "ファイルが存在します。${dotfilesDirectory}/backup/${filename}に移動します。"
-            mv "$2" "${dotfilesDirectory}/backup/${filename}"
-        fi
-    fi
-    ln -s "$1" "$2"
-}
-
 ### Yes/Noを尋ねます。未指定の場合はNoになります。
 ### @return yes: 0
 ### @return no: 1
