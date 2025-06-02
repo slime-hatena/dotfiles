74075acf3fd4d754bbab24b80be5e79089bf8d66
slime-hatena <git@aki-memo.net>
2025-06-02 17:50:47 +0900

    feat: code insiderを起動するショートカットの追加

--- env/zsh/.zsh.d/abbr.zsh
+++ env/zsh/.zsh.d/abbr.zsh
@@ -4,3 +4,4 @@ abbr --session gl="git log" 1> /dev/null
 abbr --session git\ mfeo="git fetch && git merge --no-edit origin/" 1> /dev/null
 
 abbr --session tt="tmuximum" 1> /dev/null
+abbr --session codi="code-insiders" 1> /dev/null
