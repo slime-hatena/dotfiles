c95339a918953ca9a3c0b6b6dc882439482d481d
slime-hatena <git@aki-memo.net>
2026-05-15 17:01:47 +0900

    fix: abbrコマンドに--quietオプションを追加

--- dot_zsh.d/abbr.zsh
+++ dot_zsh.d/abbr.zsh
@@ -1,10 +1,10 @@
-abbr --session ll="ls -la" 1> /dev/null
-abbr --session llt="ls -lat" 1> /dev/null
-abbr --session gl="git log" 1> /dev/null
-abbr --session git\ mfeo="git fetch && git merge --no-edit origin/" 1> /dev/null
+abbr --session --quiet ll="ls -la" 1> /dev/null
+abbr --session --quiet llt="ls -lat" 1> /dev/null
+abbr --session --quiet gl="git log" 1> /dev/null
+abbr --session --quiet git\ mfeo="git fetch && git merge --no-edit origin/" 1> /dev/null
 
-abbr --session tt="tmuximum" 1> /dev/null
-abbr --session codi="code-insiders" 1> /dev/null
+abbr --session --quiet tt="tmuximum" 1> /dev/null
+abbr --session --quiet codi="code-insiders" 1> /dev/null
 
-abbr --session cddotfile="cd ~/.local/share/chezmoi" 1> /dev/null
-abbr --session codedotfile="code ~/.local/share/chezmoi" 1> /dev/null
+abbr --session --quiet cddotfile="cd ~/.local/share/chezmoi" 1> /dev/null
+abbr --session --quiet codedotfile="code ~/.local/share/chezmoi" 1> /dev/null
