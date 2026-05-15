dd9f85b42f6b25c66fbd0d12c604d00c7180d1be
slime-hatena <git@aki-memo.net>
2026-05-15 17:02:08 +0900

    fix: abbrコマンドから出力リダイレクトを削除

--- dot_zsh.d/abbr.zsh
+++ dot_zsh.d/abbr.zsh
@@ -1,10 +1,10 @@
-abbr --session --quiet ll="ls -la" 1> /dev/null
-abbr --session --quiet llt="ls -lat" 1> /dev/null
-abbr --session --quiet gl="git log" 1> /dev/null
-abbr --session --quiet git\ mfeo="git fetch && git merge --no-edit origin/" 1> /dev/null
+abbr --session --quiet ll="ls -la"
+abbr --session --quiet llt="ls -lat"
+abbr --session --quiet gl="git log"
+abbr --session --quiet git\ mfeo="git fetch && git merge --no-edit origin/"
 
-abbr --session --quiet tt="tmuximum" 1> /dev/null
-abbr --session --quiet codi="code-insiders" 1> /dev/null
+abbr --session --quiet tt="tmuximum"
+abbr --session --quiet codi="code-insiders"
 
-abbr --session --quiet cddotfile="cd ~/.local/share/chezmoi" 1> /dev/null
-abbr --session --quiet codedotfile="code ~/.local/share/chezmoi" 1> /dev/null
+abbr --session --quiet cddotfile="cd ~/.local/share/chezmoi"
+abbr --session --quiet codedotfile="code ~/.local/share/chezmoi"
