c71935f20b3318541a730bffce84630432ec532b
slime-hatena <git@aki-memo.net>
2026-05-15 17:37:22 +0900

    fix: abbrコマンドのgit mfeoエイリアスを修正し、エイリアスのマーカーを追加

--- dot_zsh.d/abbr.zsh
+++ dot_zsh.d/abbr.zsh
@@ -1,7 +1,10 @@
+ABBR_SET_EXPANSION_CURSOR=1
+ABBR_EXPANSION_CURSOR_MARKER='%'
+
 abbr --session --quiet ll="ls -la"
 abbr --session --quiet llt="ls -lat"
 abbr --session --quiet gl="git log"
-abbr --session --quiet git\ mfeo="git fetch && git merge --no-edit origin/"
+abbr --session --quiet "git mfeo"="git fetch && git merge --no-edit origin/%"
 
 abbr --session --quiet tt="tmuximum"
 abbr --session --quiet codi="code-insiders"
