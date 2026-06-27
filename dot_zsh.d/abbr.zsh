0dfff7442a001755ac1b704eb53135ccfeeaa0a1
slime-hatena <git@aki-memo.net>
2026-06-28 04:52:48 +0900

    feat: 新しいコマンドエイリアス ccl と ccr を追加

--- dot_zsh.d/abbr.zsh
+++ dot_zsh.d/abbr.zsh
@@ -11,3 +11,6 @@ abbr --session --quiet codi="code-insiders"
 
 abbr --session --quiet .cd="cd ~/.local/share/chezmoi"
 abbr --session --quiet .code="code ~/.local/share/chezmoi"
+
+abbr --session --quiet ccl="brew upgrade claude-code@latest; claude --dangerously-skip-permissions"
+abbr --session --quiet ccr="brew upgrade claude-code@latest; claude --dangerously-skip-permissions -r"
